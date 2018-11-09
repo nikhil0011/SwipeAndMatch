a0da6cf8102b1b6c56330d8e0cfbeeeeef60012a
nikhil <nikhil.nangia85@gmail.com>
2018-11-10 00:08:06 +0530

    Initial Commit

---
+++ SwipeAndMatch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwipeAndMatch
+//
+//  Created by Admin on 11/10/18.
+//  Copyright © 2018 Travelogy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
